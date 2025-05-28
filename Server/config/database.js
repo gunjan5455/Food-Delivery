1a937fcdfe3f5dfe579bb2a851f965befac616d0
gunjan5455 <gunjankotadiya5455@gmail.com>
2025-05-28 18:00:50 +0530

    env removed

--- Server/config/database.js
+++ Server/config/database.js
@@ -3,10 +3,6 @@ import userModel from "../models/userModel.js";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://gunjankotadiya5455:tN0OsfcUB1ARD7Et@cluster0.hljnj.mongodb.net/Food_Delivery?retryWrites=true&w=majority&appName=Cluster0"
-    )
+    .connect(process.env.DB_URL)
     .then(() => console.log("Connected to DB"));
 };
-
-// "mongodb+srv://gunjankotadiya5455:tN0OsfcUB1ARD7Et@cluster0.hljnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
